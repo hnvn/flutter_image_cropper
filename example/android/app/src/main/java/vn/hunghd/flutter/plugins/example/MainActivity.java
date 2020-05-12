ad439e0ddd3706981734ad2835c30aa97f040b77
Fabio de Matos <fmatosqg@gmail.com>
2020-05-12 17:31:31 +1000

    Fix crash after opening app with deeplink

--- example/android/app/src/main/java/vn/hunghd/flutter/plugins/example/MainActivity.java
+++ example/android/app/src/main/java/vn/hunghd/flutter/plugins/example/MainActivity.java
@@ -1,13 +1,18 @@
 package vn.hunghd.flutter.plugins.example;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import vn.hunghd.flutter.plugins.imagecropper.ImageCropperPlugin;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
-  }
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+
+        flutterEngine.getPlugins().add(new ImageCropperPlugin());
+    }
 }
