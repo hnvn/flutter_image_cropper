420df779b67e2243a07ea92d7eeb57ee6a06434e
hnvn <hunghd.yb@gmail.com>
2023-05-14 16:17:04 +0700

    migrate to gradle 8

--- image_cropper/android/src/main/java/vn/hunghd/flutter/plugins/imagecropper/ImageCropperDelegate.java
+++ image_cropper/android/src/main/java/vn/hunghd/flutter/plugins/imagecropper/ImageCropperDelegate.java
@@ -242,7 +242,7 @@ public class ImageCropperDelegate implements PluginRegistry.ActivityResultListen
         if ("square".equals(name)) {
             return new AspectRatio(null, 1.0f, 1.0f);
         } else if ("original".equals(name)) {
-            return new AspectRatio(activity.getString(R.string.ucrop_label_original).toUpperCase(),
+            return new AspectRatio(activity.getString(com.yalantis.ucrop.R.string.ucrop_label_original).toUpperCase(),
                     CropImageView.SOURCE_IMAGE_ASPECT_RATIO, 1.0f);
         } else if ("3x2".equals(name)) {
             return new AspectRatio(null, 3.0f, 2.0f);
@@ -257,7 +257,7 @@ public class ImageCropperDelegate implements PluginRegistry.ActivityResultListen
         } else if ("16x9".equals(name)) {
             return new AspectRatio(null, 16.0f, 9.0f);
         } else {
-            return new AspectRatio(activity.getString(R.string.ucrop_label_original).toUpperCase(),
+            return new AspectRatio(activity.getString(com.yalantis.ucrop.R.string.ucrop_label_original).toUpperCase(),
                     CropImageView.SOURCE_IMAGE_ASPECT_RATIO, 1.0f);
         }
     }
