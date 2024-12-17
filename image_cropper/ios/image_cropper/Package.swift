17f742129de725aee802f4ce6caa3d33ba039de3
hnvn <hunghd.yb@gmail.com>
2024-12-17 10:15:25 +0700

    initialize new swift package configuration

---
+++ image_cropper/ios/image_cropper/Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "image_cropper",
+    platforms: [
+        .iOS("12.0")
+    ],
+    products: [
+        .library(name: "image_cropper", targets: ["image_cropper"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "image_cropper",
+            dependencies: [],
+            resources: [
+                // TODO: If your plugin requires a privacy manifest
+                // (e.g. if it uses any required reason APIs), update the PrivacyInfo.xcprivacy file
+                // to describe your plugin's privacy impact, and then uncomment this line.
+                // For more information, see:
+                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
+                // .process("PrivacyInfo.xcprivacy"),
+
+                // TODO: If you have other resources that need to be bundled with your plugin, refer to
+                // the following instructions to add them:
+                // https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
+            ]
+        )
+    ]
+)
